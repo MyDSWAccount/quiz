44e24b1ae3b8ad9e35095c80893d903ea0ff61bf
MyDSWAccount <77691217+MyDSWAccount@users.noreply.github.com>
2021-02-03 10:08:50 -0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
         var n;
-        for (n = 1; n < 7; n++) {
-                $("#q" + n).click(function(){
-                        $("#a" + n).show();
-                        $("#q" + (n+1)).show();
+        $("#q" + n).click(function(){
+                $("#a" + n).show();
+                $("#q" + (n+1)).show();
         });
-        }
 });
